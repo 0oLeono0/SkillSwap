a36b5e2773de662fe751ada9ae0c5767c23c74e2
unknown <lyonya_ivanovskij@mail.ru>
2026-05-13 19:59:35 +0300

    refactor: add app providers public api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './styles/index.css';
-import { AppProviders } from '@/app/providers/AppProviders';
-import { router } from '@/app/providers/routes';
+import { AppProviders, router } from '@/app/providers';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/app/providers/index.ts
@@ -0,0 +1,5 @@
+export { AppProviders } from './AppProviders';
+export * from './auth';
+export * from './favorites';
+export * from './routes';
+export * from './theme';
