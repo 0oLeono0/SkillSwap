577a94a60e41862469a44379508522720e1b0a7a
unknown <lyonya_ivanovskij@mail.ru>
2026-05-13 19:55:42 +0300

    refactor: add routes provider public api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './styles/index.css';
-import { router } from '@/app/providers/routes/AppRouter';
+import { router } from '@/app/providers/routes';
 import { ThemeProvider } from '@/app/providers/theme/ThemeProvider';
 import { AuthProvider } from '@/app/providers/auth/AuthProvider';
 import { FavoritesProvider } from '@/app/providers/favorites/FavoritesProvider';
@@ -16,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
         </FavoritesProvider>
       </AuthProvider>
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 );
---
+++ src/app/providers/routes/index.ts
@@ -0,0 +1,7 @@
+export { router } from './AppRouter';
+export { authRoutes } from './authRoutes';
+export { errorRoutes } from './errorRoutes';
+export { mainRoutes } from './mainRoutes';
+export { profileRoutes } from './profileRoutes';
+export { rootRoute } from './rootRoute';
+export { RouteStub } from './RouteStub';
